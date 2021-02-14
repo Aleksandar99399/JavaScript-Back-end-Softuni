6f4c4607a646038c0b6fb68dbc6ac65e97edebfc
Aleksandar99399 <aleksandar_unchev@abv.bg>
2021-02-14 15:57:24 +0000

    "cat shelter"

--- 01. Cat-Shelter-Workshop-Resources/index.js
+++ 01. Cat-Shelter-Workshop-Resources/index.js
@@ -1,42 +1,59 @@
-const fs = require('fs');
-const url = require('url')
-const http = require('http');
+const fs = require("fs");
+const url = require("url");
+const http = require("http");
 
-function replaceHead(page){
-    let tem = page.replace('{%HEADTEMP%}',headTemp); 
-    return tem;
+function replaceHead(page) {
+  let tem = page.replace("{%HEADTEMP%}", headTemp);
+  return tem;
 }
-function replaceNav(page){
-    let tem = page.replace('{%NAVTEMP%}',navTemp);
-    return tem;
+function replaceNav(page) {
+  let tem = page.replace("{%NAVTEMP%}", navTemp);
+  return tem;
 }
 
+const readIndexPage = fs.readFileSync(
+  `${__dirname}/resources/views/home/index.html`,
+  "utf-8"
+);
+const readAddBreedPage = fs.readFileSync(
+  `${__dirname}/resources/views/addBreed.html`,
+  "utf-8"
+);
+const readAddCatPage = fs.readFileSync(
+  `${__dirname}/resources/views/addCat.html`,
+  "utf-8"
+);
+const headTemp = fs.readFileSync(
+  `${__dirname}/resources/views/templates/headTemp.html`,
+  "utf-8"
+);
+const navTemp = fs.readFileSync(
+  `${__dirname}/resources/views/templates/navTemp.html`,
+  "utf-8"
+);
 
-const readIndexPage = fs.readFileSync(`${__dirname}/resources/views/home/index.html`,'utf-8')
-const readAddBreedPage = fs.readFileSync(`${__dirname}/resources/views/addBreed.html`, 'utf-8')
-const headTemp = fs.readFileSync(`${__dirname}/resources/views/templates/headTemp.html`,'utf-8')
-const navTemp = fs.readFileSync(`${__dirname}/resources/views/templates/navTemp.html`,'utf-8')
-
-
-http.createServer((req,res)=>{
-
+http
+  .createServer((req, res) => {
     const path = url.parse(req.url).pathname;
-    
+
     if (path === "/") {
-        res.writeHead(200,{
-            'Content-type': 'text/html'
-        })
-        
-        res.end(replaceNav(replaceHead(readIndexPage)));
-    }else if (path === '/addBreed') {
-        
-        res.writeHead(200,{
-            'Content-type': 'text/html'
-        })
+      res.writeHead(200, {
+        "Content-type": "text/html",
+      });
 
+      res.end(replaceNav(replaceHead(readIndexPage)));
+    } else if (path === "/addBreed") {
+      res.writeHead(200, {
+        "Content-type": "text/html",
+      });
 
-        res.end(replaceTemp(readAddBreedPage));
+      res.end(replaceNav(replaceHead(readAddBreedPage)));
+    } else if (path === "/addCat") {
+      res.writeHead(200, {
+        "Content-type": "text/html",
+      });
+
+      res.end(replaceNav(replaceHead(readAddCatPage)));
     }
-
-    
-}).listen(5000)
+  })
+  .listen(5000);
